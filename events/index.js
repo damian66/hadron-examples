8e9b027d4f968a79fa0d6927f16e01f8bb904a93
damian66 <damian95@me.com>
2018-06-06 10:27:36 +0200

    hadron-events initial commit

---
+++ events/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import hadron from '@brainhubeu/hadron-core';
+
+const hadronExpress = require('@brainhubeu/hadron-express');
+const hadronEvents = require('@brainhubeu/hadron-events');
+
+const app = new express();
+const port = process.env.PORT || 3000;
+
+hadron(
+  app,
+  [hadronExpress],
+  {
+    routes: {
+      helloWorld: {
+        path: '/',
+        methods: ['GET'],
+        callback: () => 'Hello world !',
+      }
+    }
+  }
+).then(container => {
+  app.listen(port, () =>
+    console.log(`Listening on http://localhost:${port}`),
+  );
+})
