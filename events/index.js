d8f46f78a97d9d84384836233db51f229f20b2d8
damian66 <damian95@me.com>
2018-06-06 11:36:53 +0200

    listeners

--- events/index.js
+++ events/index.js
@@ -1,25 +1,66 @@
 import express from 'express';
 import hadron from '@brainhubeu/hadron-core';
 
-const hadronExpress = require('@brainhubeu/hadron-express');
 const hadronEvents = require('@brainhubeu/hadron-events');
+const hadronExpress = require('@brainhubeu/hadron-express');
 
 const app = new express();
 const port = process.env.PORT || 3000;
 
 hadron(
   app,
-  [hadronExpress],
+  [hadronEvents, hadronExpress],
   {
     routes: {
       helloWorld: {
         path: '/',
         methods: ['GET'],
-        callback: () => 'Hello world !',
+        callback: () => 'Check server\'s console !',
       }
+    },
+    events: {
+      listeners: [
+        {
+          name: 'Test',
+          event: 'handleRequestCallbackEvent',
+          handler: (data, ...rest) => {
+            console.log('handleRequestCallbackEvent', data, ...rest);
+            data();
+          }
+        },
+        {
+          name: 'adsa',
+          event: 'handleInitializeApplicationEvent',
+          handler: (data, ...rest) => {
+            console.log('handleInitializeApplicationEvent', data, ...rest);
+            data();
+          }
+        },
+        {
+          name: 'dadada',
+          event: 'handleTerminateApplicationEvent',
+          handler: (data, ...rest) => {
+            console.log('handleTerminateApplicationEvent', data, ...rest);
+            data();
+          }
+        },
+        {
+          name: 'custom',
+          event: 'customEvent',
+          handler: (cb) => {
+            console.log('???');
+            cb();
+          }
+        },
+      ],
     }
   }
 ).then(container => {
+  const eventManager = container.take('eventManager');
+  // eventManager.registerEvents([]);
+
+  eventManager.emitEvent('customEvent');
+
   app.listen(port, () =>
     console.log(`Listening on http://localhost:${port}`),
   );
